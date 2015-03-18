cff3429437ece4ae9c384f125c19cd0ad45f1b54
Mateusz Łukomski <matrix.disc@gmail.com>
2015-03-18 12:08:58 +0100

    Miniscule fixes of ReSharper warnings

--- Game Challenge 1/Source.cpp
+++ Game Challenge 1/Source.cpp
@@ -9,21 +9,21 @@ using namespace std;
 
 int main()
 {
-	mt19937 randomGenerator(time(NULL));
+	mt19937 randomGenerator(time(nullptr));
 
 	UnitStats humanStats = { "human", 100, 12, 23, 0.0, 1.0, 80 };
 	UnitStats orcStats = { "orc", 120, 16, 30, 0.0, 1.0, 70 };
 	UnitStats monsterStats = { "monster", 2000, 73, 127, 0.0, 1.0, 60 };
-	
-	Unit monster = Unit(monsterStats, randomGenerator);
-	Unit human = Unit(humanStats, randomGenerator);
-	Unit orc = Unit(orcStats, randomGenerator);
+
+	auto monster = Unit(monsterStats, randomGenerator);
+	auto human = Unit(humanStats, randomGenerator);
+	auto orc = Unit(orcStats, randomGenerator);
 
 	int attackValue;
-	bool isDead = false;
-	
-	Unit attacker = human;
-	Unit defender = orc;
+	auto isDead = false;
+
+	auto attacker = human;
+	auto defender = orc;
 
 	while(!isDead)
 	{
@@ -38,7 +38,7 @@ int main()
 		else 
 		{
 			cout << defender.name << " is left with " << hpLeft << " HP" << endl;
-			Unit tmp = attacker;
+			auto tmp = attacker;
 			attacker = defender;
 			defender = tmp;
 		}
